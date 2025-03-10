023cb867a782d05e7b7073c14ab9691395335056
samuel <samawyck@gmail.com>
2025-03-10 11:56:10 -0500

    Create hashmap for weather icons

---
+++ src/weatherIcons.js
@@ -0,0 +1,50 @@
+import clearDay from "./imgs/weather-icons/clear-day.png";
+import clearNight from "./imgs/weather-icons/clear-night.png";
+import cloudy from "./imgs/weather-icons/cloudy.png";
+import fog from "./imgs/weather-icons/fog.png";
+import hail from "./imgs/weather-icons/hail.png";
+import partlyCloudyDay from "./imgs/weather-icons/partly-cloudy-day.png";
+import partlyCloudyNight from "./imgs/weather-icons/partly-cloudy-night.png";
+import rainSnowShowersDay from "./imgs/weather-icons/rain-snow-showers-day.png";
+import rainSnowShowersNight from "./imgs/weather-icons/rain-snow-showers-night.png";
+import rainSnow from "./imgs/weather-icons/rain-snow.png";
+import rain from "./imgs/weather-icons/rain.png";
+import showersDay from "./imgs/weather-icons/showers-day.png";
+import showersNight from "./imgs/weather-icons/showers-night.png";
+import sleet from "./imgs/weather-icons/sleet.png";
+import snowShowersDay from "./imgs/weather-icons/snow-showers-day.png";
+import snowShowersNight from "./imgs/weather-icons/snow-showers-night.png";
+import snow from "./imgs/weather-icons/snow.png";
+import thunderRain from "./imgs/weather-icons/thunder-rain.png";
+import thunderShowersDay from "./imgs/weather-icons/thunder-showers-day.png";
+import thunderShowersNight from "./imgs/weather-icons/thunder-showers-night.png";
+import thunder from "./imgs/weather-icons/thunder.png";
+import wind from "./imgs/weather-icons/wind.png";
+
+
+const icons = {
+    "clear-day": clearDay,
+    "clear-night": clearNight,
+    "cloudy": cloudy,
+    "fog": fog,
+    "hail": hail,
+    "partly-cloudy-day": partlyCloudyDay,
+    "partly-cloudy-night": partlyCloudyNight,
+    "rain-snow-showers-day": rainSnowShowersDay,
+    "rain-snow-showers-night": rainSnowShowersNight,
+    "rain-snow": rainSnow,
+    "rain": rain,
+    "showers-day": showersDay,
+    "showers-night": showersNight,
+    "sleet": sleet,
+    "snow-showers-day": snowShowersDay,
+    "snow-showers-night": snowShowersNight,
+    "snow": snow,
+    "thunder-rain": thunderRain,
+    "thunder-showers-day": thunderShowersDay,
+    "thunder-showers-night": thunderShowersNight,
+    "thunder": thunder,
+    "wind": wind,
+};
+
+export default icons;
